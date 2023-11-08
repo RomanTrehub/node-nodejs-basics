db9f58c158e53ef3e0baabf2747771e2f4a7aa7b
Roman <roman.tg92@gmail.com>
2023-11-08 20:35:17 +0200

    add rename functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { rename as renamePath } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const OLD_PATH = `${__dirname}/files/wrongFilename.txt`;
+const NEW_PATH = `${__dirname}/files/properFilename.md`;
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await renamePath(OLD_PATH, NEW_PATH);
+  } catch (e) {
+    console.log("FS operation failed");
+  }
 };
 
 await rename();
