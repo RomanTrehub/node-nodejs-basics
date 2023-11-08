72c82bb4406351d2b6ed1a2e80332cc9ba318db7
Roman <roman.tg92@gmail.com>
2023-11-08 19:47:47 +0200

    add copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { copyFile, opendir, mkdir } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CURRENT_DIRECTORY_PATH = `${__dirname}/files`;
+const NEW_DIRECTION_PATH = `${__dirname}/files_copy`;
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await mkdir(NEW_DIRECTION_PATH);
+
+    const dir = await opendir(CURRENT_DIRECTORY_PATH);
+    for await (const { name, path } of dir) {
+      await copyFile(`${path}/${name}`, `${NEW_DIRECTION_PATH}/${name}`);
+    }
+  } catch (e) {
+    console.log("FS operation failed");
+  }
 };
 
 await copy();
