2e350b2e56d488c8070c6ac4735e82dd7c25ba02
Roman <roman.tg92@gmail.com>
2023-11-08 21:09:09 +0200

    add removal functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { rm } from "node:fs/promises";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILEPATH_TO_REMOVE = `${__dirname}/files/fileToRemove.txt`;
+
+const remove = async () => {
+  try {
+    await rm(FILEPATH_TO_REMOVE);
+  } catch (e) {
+    console.log("FS operation failed");
+  }
+};
 await remove();
