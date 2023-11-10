b4a7e0f985220e97fab6d87cfe9f963b43afd5cb
Roman <roman.tg92@gmail.com>
2023-11-10 20:39:04 +0200

    add args functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,30 @@
+import { argv } from "node:process";
+
+const mapToStringConcat = (map) => {
+  let string = "";
+
+  map.forEach((val, key) => {
+    string += `${key} is ${val}, `;
+  });
+
+  return string;
+};
+
 const parseArgs = () => {
-    // Write your code here 
+  const parsedValues = new Map();
+
+  argv.forEach((val, i) => {
+    if (val.startsWith("--")) {
+      const key = val.slice(2);
+      if (argv[i + 1].startsWith("--")) {
+        parsedValues.set(key, null);
+      } else {
+        parsedValues.set(key, argv[i + 1]);
+      }
+    }
+  });
+
+  console.log(mapToStringConcat(parsedValues));
 };
 
 parseArgs();
