a67ce51b512d12c1ac8867dc2fe87a52b8e346fe
Roman <roman.tg92@gmail.com>
2023-11-13 19:29:06 +0200

    add hash functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILE_PATH = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  hash.setEncoding("hex");
+  const fileStream = createReadStream(FILE_PATH);
+
+  fileStream.on("end", () => {
+    hash.end();
+    console.log(hash.read());
+  });
+
+  fileStream.pipe(hash);
 };
 
 await calculateHash();
