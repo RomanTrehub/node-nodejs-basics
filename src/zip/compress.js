83ace2cd4f63c0d5d79e36bf131beb7f43398b94
Roman <roman.tg92@gmail.com>
2023-11-15 19:38:24 +0200

    add zlib functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { rm } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import { promisify } from "node:util";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILES_PATH = `${__dirname}/files`;
+const INPUT_PATH = `${FILES_PATH}/fileToCompress.txt`;
+const OUTPUT_PATH = `${FILES_PATH}/archive.gz`;
+
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(INPUT_PATH);
+  const writeStream = createWriteStream(OUTPUT_PATH);
+  const compressor = createGzip();
+
+  try {
+    await pipe(readStream, compressor, writeStream);
+    await rm(INPUT_PATH);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import { promisify } from "node:util";
+import { rm } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILES_PATH = `${__dirname}/files`;
+const INPUT_PATH = `${FILES_PATH}/archive.gz`;
+const OUTPUT_PATH = `${FILES_PATH}/fileToCompress.txt`;
+
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(INPUT_PATH);
+  const writeStream = createWriteStream(OUTPUT_PATH);
+  const deCompressor = createGunzip();
+
+  try {
+    await pipe(readStream, deCompressor, writeStream);
+    await rm(INPUT_PATH);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await decompress();
