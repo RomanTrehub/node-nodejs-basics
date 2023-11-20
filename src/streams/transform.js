11286e9786bbdebe09804537a75d6abaa6c51788
Roman Trehub <roman.tg92@gmail.com>
2023-11-20 21:33:18 +0200

    add EOL to transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,11 +1,12 @@
 import { openStdin, stdout } from "process";
+import { EOL } from "os";
 import { Transform } from "stream";
 
 class ReverseStream extends Transform {
-  _transform(chunk, encoding, cb) {
+  _transform(chunk, _, cb) {
     const result = `${chunk.toString("utf8")}`.split("").reverse().join("");
     try {
-      cb(null, result);
+      cb(null, `${result} ${EOL}`);
     } catch (e) {
       cb(e);
     }
