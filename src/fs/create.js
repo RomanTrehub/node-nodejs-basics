e48026908183c07ae970b3d7c269dd0bb7d41581
Roman <roman.tg92@gmail.com>
2023-11-20 19:44:02 +0200

    improve create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { appendFile, stat } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,20 +10,9 @@ const FILE_PATH = path.join(`${__dirname}/files`, FILE_NAME);
 
 const create = async () => {
   try {
-    const fileStat = await stat(FILE_PATH);
-    if (fileStat) {
-      throw new Error("Fs operation was failed: file already exists");
-    }
-  } catch (e) {
-    if (e.code === "ENOENT") {
-      try {
-        await appendFile(FILE_PATH, "I am fresh and young");
-      } catch {
-        console.log(e);
-      }
-    } else {
-      console.log(e);
-    }
+    await writeFile(FILE_PATH, "I am fresh and young", { flag: "wx" });
+  } catch {
+    throw new Error("FS operation failed");
   }
 };
 
