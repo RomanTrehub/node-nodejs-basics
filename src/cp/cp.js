73ed4c6c8418ed41d4e927e3a6b8c4e1714a1a46
Roman <roman.tg92@gmail.com>
2023-11-20 19:25:30 +0200

    add child process functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "node:child_process";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const SCRIPT_PATH = `${__dirname}/files/script`;
+
+const spawnChildProcess = async (...args) => {
+  fork(SCRIPT_PATH, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("123", 12, 1222);
