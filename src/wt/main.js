dfaedf8b1f8e3a17780b4f7a7311ad6e6d6cf0ed
Roman Trehub <roman.tg92@gmail.com>
2023-11-20 21:59:44 +0200

    improve worker thread`s error processing

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";
 import { cpus } from "node:os";
-import { Worker, workerData } from "node:worker_threads";
+import { Worker } from "node:worker_threads";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +13,7 @@ const createWorker = (workerData) => {
   return new Promise((resolve, reject) => {
     const worker = new Worker(WORKER_PATH, { workerData });
     worker.on("message", (data) => resolve({ status: "resolved", data }));
-    worker.on("error", () => resolve({ status: "error", data }));
+    worker.on("error", () => resolve({ status: "error", data: null }));
     worker.on("exit", (code) => {
       if (code !== 0) {
         reject(new Error(`Error stopped with code ${code}`));
